0b7d47026c384beb20a61dee37374d1a4a82bce0
Jesslyn Nathania <jesslynnathania99@gmail.com>
2020-02-18 15:20:19 +0700

    implements page of admin and sections landing page

---
+++ src/components/material-ui/CustomTable.js
@@ -0,0 +1,216 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import CustomSettingButton from './CustomSettingButton';
+
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map(el => el[0]);
+}
+
+function EnhancedTableHead(props) {
+    const { headCells, order, orderBy, onRequestSort } = props;
+    const createSortHandler = property => event => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map(headCell => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'default'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            href=""
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+};
+
+const useToolbarStyles = makeStyles(theme => ({
+    root: {
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(1),
+    },
+    title: {
+        flex: '1 1 100%',
+    },
+}));
+
+const EnhancedTableToolbar = (props) => {
+    const classes = useToolbarStyles();
+    const { title } = props;
+
+    return (
+        <Toolbar
+            className={clsx(classes.root, null)}
+        >
+            <Typography component="div" className={classes.title} variant="h6" id="tableTitle">
+                {title}
+            </Typography>
+            <Tooltip title="Filter list">
+                <IconButton href="" aria-label="filter list">
+                    <FilterListIcon/>
+                </IconButton>
+            </Tooltip>
+        </Toolbar>
+    );
+};
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        margin: theme.spacing(5),
+    },
+    paper: {
+        width: '100%',
+        margin: 'auto',
+        marginBottom: theme.spacing(2),
+        minWidth: theme.spacing(90),
+        maxWidth: theme.spacing(120),
+    },
+}));
+
+export default function EnhancedTable(props) {
+    const { title, headCells, rows } = props;
+    const classes = useStyles();
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('calories');
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = event => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+    return (
+        <div className={classes.root}>
+            <Paper className={classes.paper}>
+                <EnhancedTableToolbar title={title}/>
+                <TableContainer>
+                    <Table
+                        aria-labelledby="tableTitle"
+                        size="small"
+                        aria-label="enhanced table"
+                    >
+                        <EnhancedTableHead
+                            classes={classes}
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                            headCells={headCells}
+                        />
+                        <TableBody>
+                            {stableSort(rows, getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row, index) => {
+                                    const labelId = `enhanced-table-checkbox-${index}`;
+
+                                    return (
+                                        <TableRow
+                                            hover
+                                            tabIndex={-1}
+                                            key={row.username}
+                                        >
+                                            {Object.keys(row).map((k, index) => (index === 0 ?
+                                                    <TableCell component="th" id={labelId} scope="row">
+                                                        {row[k]}
+                                                    </TableCell>
+                                                    :
+                                                    <TableCell align="left">{row[k]}</TableCell>
+                                            ))}
+                                            <TableCell align="center">
+                                                <CustomSettingButton/>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            {emptyRows > 0 && (
+                                <TableRow style={{ height: 61 * emptyRows }}>
+                                    <TableCell colSpan={6}/>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </div>
+    );
+}
---
+++ src/scenes/sections-ui/SectionPage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import CustomTable from 'components/material-ui/CustomTable';
+import CustomNavbar from 'components/material-ui/CustomNavbar';
+
+function createData(id, name, context) {
+    return { id, name, context };
+}
+
+const title = 'List of Sections';
+
+const headCells = [
+    { id: 'id', numeric: false, disablePadding: false, label: 'Id' },
+    { id: 'name', numeric: false, disablePadding: false, label: 'Name' },
+    { id: 'context', numeric: false, disablePadding: false, label: 'Context' },
+    { id: 'setting', numeric: false, disablePadding: false, label: '' },
+];
+
+const rows = [
+    createData('Section1', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section2', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section3', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section4', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section5', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section6', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section7', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section8', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section9', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section10', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section11', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section12', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+    createData('Section13', 'Perumahan #1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula urna rutrum risus pretium...'),
+];
+
+
+export default function SectionPage() {
+    return (
+        <div>
+            <CustomNavbar/>
+            <CustomTable title={title} headCells={headCells} rows={rows}/>
+        </div>
+    );
+}
