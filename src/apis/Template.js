b383493646e4cdb55f717ff9bc2a34c0a3864146
imdar1 <13517002@std.stei.itb.ac.id>
2020-03-27 16:52:06 +0700

    Edit API Call

--- src/apis/Template.js
+++ src/apis/Template.js
@@ -18,13 +18,17 @@ const addNewTemplate = sectionData => {
     });
 };
 
-const getAllTemplates = () => {
+const getAllTemplates = (currPage = 1, perPage = 6, find = null) => {
     return new Promise((resolve, reject) => {
         axios({
             method: 'get',
             baseURL: process.env.REACT_APP_BACKEND_URL,
-            url: '/template/all',
+            url: '/template/' + currPage,
             withCredentials: true,
+            params: {
+                perpage: perPage,
+                find: find,
+            },
         })
             .then(({data}) => {
                 resolve(data);
@@ -36,4 +40,21 @@ const getAllTemplates = () => {
     });
 };
 
-export { addNewTemplate, getAllTemplates };
+const deleteTemplateById = id => {
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'delete',
+            baseURL: process.env.REACT_APP_BACKEND_URL,
+            url: '/template/' + id,
+            withCredentials: true,
+        })
+            .then(({ data }) => {
+                resolve(data);
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+};
+
+export { addNewTemplate, getAllTemplates, deleteTemplateById };
