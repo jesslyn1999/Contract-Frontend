62371aa4d0485060a27aec6cca524599ef7f9dc7
naufalzhafran <latifnaufal7@gmail.com>
2020-04-16 16:53:03 +0700

    add kontrak page

--- src/scenes/jamlak-ui/JamlakPage.js
+++ src/scenes/jamlak-ui/JamlakPage.js
@@ -1,94 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { withRouter, useHistory } from 'react-router-dom';
-import CustomTableJamlak from 'components/material-ui/CustomTableJamlak';
+import { withRouter } from 'react-router-dom';
 import CustomNavbar from 'components/material-ui/CustomNavbar';
-import apis from 'apis';
-import { parse, stringify } from 'qs';
-import useTheme from '@material-ui/core/styles/useTheme';
 import themePage from 'scenes/theme';
+import JamlakTable from 'scenes/jamlak-ui/table/JamlakTable';
 
-function JamlakPage(props) {
-    const theme = useTheme();
-    const history = useHistory();
-    const [isLoading, setIsLoading] = useState(false);
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
-    const [totalPages, setTotalPages] = useState(0);
-    const [rows, setRows] = useState([]);
-    const [query, setQuery] = useState(parse(props.location.search, { ignoreQueryPrefix: true }));
-
-    const title = 'List of Jaminan Pelaksana';
-
-    const headCells = [
-        { id: 'no_jamlak', numeric: false, disablePadding: false, label: 'No' },
-        { id: 'tgl_pembuatan', numeric: false, disablePadding: false, label: 'Tanggal Pembuatan' },
-        { id: 'nama_bank', numeric: false, disablePadding: false, label: 'Nama Bank' },
-        { id: 'no_sppbj', numeric: false, disablePadding: false, label: 'No SPPBJ' },
-        { id: 'setting', numeric: false, disablePadding: false, label: '' },
-    ];
-
-    const columnWidths = [theme.spacing(0), theme.spacing(20)];
-
-    const handleSearch = find => event => {
-        event.preventDefault();
-        history.push({
-            pathname: '/jamlak',
-            search: stringify({
-                find: find,
-            }),
-        });
-    };
-
-    useEffect(() => {
-        setQuery(parse(props.location.search, { ignoreQueryPrefix: true }));
-    }, [props.location]);
-
-    useEffect(() => {
-        const fetchJamlaks = async (currPage, perPage, find) => {
-            setIsLoading(true);
-            apis.jamlak
-                .getJamlaks(currPage + 1, perPage, find)
-                .then(res => {
-                    const { data, pages } = res;
-                    let data_temp = data.map(item => ({
-                        _id: item['_id'],
-                        no_jamlak: item['no_jamlak'],
-                        tgl_pembuatan: item['tgl_pembuatan'],
-                        nama_bank: item['nama_bank'],
-                        no_sppbj: item['no_sppbj'],
-                    }));
-                    setRows(data_temp);
-                    setTotalPages(pages);
-                })
-                .catch(err => {
-                    console.log('Error in fetchJamlaks.\n', err);
-                })
-                .finally(() => {
-                    setIsLoading(false);
-                });
-        };
-        fetchJamlaks(page, rowsPerPage, query['find']);
-    }, [page, rowsPerPage, query]);
-
+function SpbbjPage() {
     return (
         <ThemeProvider theme={themePage}>
             <CustomNavbar />
-            <CustomTableJamlak
-                columnWidths={columnWidths}
-                title={title}
-                isLoading={isLoading}
-                headCells={headCells}
-                rows={rows}
-                page={page}
-                setPage={setPage}
-                rowsPerPage={rowsPerPage}
-                setRowsPerPage={setRowsPerPage}
-                totalPages={totalPages}
-                handleSearch={handleSearch}
-            />
+            <JamlakTable />
         </ThemeProvider>
     );
 }
 
-export default withRouter(JamlakPage);
+export default withRouter(SpbbjPage);
---
+++ src/scenes/jamlak-ui/setting-button/SpbbjSettingButton.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import SettingsIcon from '@material-ui/icons/Settings';
+import IconButton from '@material-ui/core/IconButton';
+import Popover from '@material-ui/core/Popover';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Link as RouterLink } from 'react-router-dom';
+
+const useStyles = makeStyles(theme => ({
+    list: {
+        width: '100%',
+        maxWidth: theme.spacing(30),
+    },
+    listItemButton: {
+        padding: theme.spacing(0, 3),
+    },
+    popover: {
+        margin: theme.spacing(0),
+        padding: theme.spacing(0),
+    },
+}));
+
+function ListItemLink(props) {
+    const { primary, to, className, rowId, rowData } = props;
+
+    const renderLink = React.useMemo(
+        () =>
+            React.forwardRef((itemProps, ref) => (
+                <RouterLink
+                    to={{
+                        pathname: to,
+                        sppbjId: rowId,
+                        sppbjData: rowData,
+                    }}
+                    ref={ref}
+                    {...itemProps}
+                />
+            )),
+        [rowId, rowData, to],
+    );
+
+    return (
+        <li>
+            <ListItem button component={renderLink} className={className}>
+                <ListItemText primary={primary} />
+            </ListItem>
+        </li>
+    );
+}
+
+ListItemLink.propTypes = {
+    primary: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
+    rowId: PropTypes.string.isRequired,
+};
+
+export default function SpbbjSettingButton(props) {
+    const { rowId, rowData } = props;
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <React.Fragment>
+            <Tooltip title="setting" enterDelay={500} leaveDelay={100} arrow>
+                <IconButton
+                    href=""
+                    aria-label="setting button"
+                    component="span"
+                    onClick={handleClick}
+                >
+                    <SettingsIcon />
+                </IconButton>
+            </Tooltip>
+            <Popover
+                id={Boolean(anchorEl) ? 'popover-setting' : undefined}
+                open={Boolean(anchorEl)}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'center',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                className={classes.popover}
+            >
+                <List component="nav" aria-label="setting options" className={classes.list}>
+                    <ListItemLink
+                        className={classes.listItemButton}
+                        primary="Form Data Jamlak"
+                        to="/jamlak-form"
+                        rowId={rowId}
+                        rowData={rowData}
+                    />
+                    <ListItemLink
+                        className={classes.listItemButton}
+                        primary="Membuat dokumen jamlak"
+                        to="/create-jamlak"
+                        rowId={rowId}
+                        rowData={rowData}
+                    />
+                </List>
+            </Popover>
+        </React.Fragment>
+    );
+}
+
+SpbbjSettingButton.propTypes = {
+    rowId: PropTypes.string.isRequired,
+};
+
